428e1d76a6a6f17a1eb09a83cf8e0bc566f19553
irides <tsujianii@tuta.io>
2023-09-29 13:39:05 +0800

    add undocumented flags

--- twilight-model/src/user/flags.rs
+++ twilight-model/src/user/flags.rs
@@ -5,6 +5,7 @@ use serde::{
 };
 
 bitflags! {
+    /// Undocumented flags from [https://github.com/lewisakura/discord-flags/blob/master/flags/user.json].
     pub struct UserFlags: u64 {
         /// Discord Employee.
         const STAFF = 1;
@@ -14,6 +15,10 @@ bitflags! {
         const HYPESQUAD = 1 << 2;
         /// Bug hunter level 1.
         const BUG_HUNTER_LEVEL_1 = 1 << 3;
+        /// User has SMS 2FA enabled.
+        const MFA_SMS = 1 << 4;
+        /// Unknown. Presumably some sort of Discord Nitro promotion that the user dismissed.
+        const PREMIUM_PROMO_DISMISSED = 1 << 5;
         /// House Bravery member.
         const HYPESQUAD_ONLINE_HOUSE_1 = 1 << 6;
         /// House Brilliance member.
@@ -24,8 +29,16 @@ bitflags! {
         const PREMIUM_EARLY_SUPPORTER = 1 << 9;
         /// User is in a team.
         const TEAM_PSEUDO_USER = 1 << 10;
+        /// An internal flag accidentally leaked to the client's private flags. Relates to partner/verification applications but nothing else is known.
+        const INTERNAL_APPLICATION = 1 << 11;
+        /// Account is a Discord system account.
+        const SYSTEM = 1 << 12;
+        /// User has unread messages from Discord.
+        const HAS_UNREAD_URGENT_MESSAGES = 1 << 13;
         /// Bug hunter level 2.
         const BUG_HUNTER_LEVEL_2 = 1 << 14;
+        /// Unused. User was deleted for being underage.
+        const UNDERAGE_DELETED = 1 << 15;
         /// Verified bot.
         const VERIFIED_BOT = 1 << 16;
         /// Early verified bot developer.
@@ -38,10 +51,40 @@ bitflags! {
         /// Bot uses only HTTP interactions and is shown in the online member
         /// list.
         const BOT_HTTP_INTERACTIONS = 1 << 19;
+        /// User is marked as a spammer.
+        const SPAMMER = 1 << 20;
+        /// Forcefully disables Nitro features.
+        const DISABLE_PREMIUM = 1 << 21;
         /// User is an [Active Developer].
         ///
         /// [Active Developer]: https://support-dev.discord.com/hc/articles/10113997751447
         const ACTIVE_DEVELOPER = 1 << 22;
+        /// Account has a high global ratelimit.
+        const HIGH_GLOBAL_RATE_LIMIT = 1 << 33;
+        /// Account has been deleted.
+        const DELETED = 1 << 34;
+        /// Account has been disabled for suspicious activity.
+        const DISABLED_SUSPICIOUS_ACTIVITY = 1 << 35;
+        /// Account was deleted by the user.
+        const SELF_DELETED = 1 << 36;
+        /// User has a premium discriminator.
+        const PREMIUM_DISCRIMINATOR = 1 << 37;
+        /// User has used the desktop client.
+        const USED_DESKTOP_CLIENT = 1 << 38;
+        /// User has used the web client.
+        const USED_WEB_CLIENT = 1 << 39;
+        /// User has used the mobile client.
+        const USED_MOBILE_CLIENT = 1 << 40;
+        /// User is currently temporarily or permanently disabled.
+        const DISABLED = 1 << 41;
+        /// User has a verified email.
+        const VERIFIED_EMAIL = 1 << 43;
+        /// User account is quarantined.
+        const QUARANTINED = 1 << 44;
+        /// User is a collaborator and has staff permissions.
+        const COLLABORATOR = 1 << 50;
+        /// User is a restricted collaborator and has staff permissions.
+        const RESTRICTED_COLLABORATOR = 1 << 51;
     }
 }
 
